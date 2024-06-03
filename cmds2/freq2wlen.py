f12c3a9135171ac93d1f6cddb61501cbd02463ae
Hüseyin YİĞİT <yigit.hsyn@gmail.com>
2024-06-03 22:23:08 +0300

    New dot notation

---
+++ cmds2/freq2wlen.py
@@ -0,0 +1,19 @@
+from scipy import constants
+
+positional_args = [
+  {
+    "name"        : "freq",       
+    "description" : "frequency in Hertz [Hz]",
+    "type"        : "float",
+    "cunt"        : 1
+  }
+]
+
+optional_args = []
+
+flags = [
+  {
+    "name" : "human",
+    "desc" : "human readable output like cm, mm, km"
+  }
+]
