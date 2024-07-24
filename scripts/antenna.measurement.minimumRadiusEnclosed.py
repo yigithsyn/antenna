167c891f911d70c10368c5bc8adc5970777a11dc
Hüseyin YİĞİT <yigit.hsyn@gmail.com>
2024-07-24 16:47:51 +0300

    generalizing inputs and output

--- scripts/antenna.measurement.minimumRadiusEnclosed.py
+++ scripts/antenna.measurement.minimumRadiusEnclosed.py
@@ -1,5 +1,5 @@
-import sys, os, argparse
-import math
+import sys, os, argparse, prettytable
+import math, numpy
 
 snme = "ante.meas.mren"                                   # short name
 lnme = "antenna.measurement.minimumRadiusEnclosed"        # long name
@@ -21,7 +21,9 @@ parg = [                                                  # positional arguments
   {"name": "dz", "desc": "max displacement from rotation center along z-axis", "type": float, "cont": 1}
 ]
 oarg = []                                                 # optional arguments
-flag = []                                                 # flags
+flag = [                                                  # flags
+  {"name": "human", "desc": "human readable output like mm, cm, m"}
+]
   
 # preparation for parsing 
 flst = []                                                 # function list
@@ -58,12 +60,30 @@ for item in oarg:
   pars.add_argument("--"+item["name"], help=item["desc"], type=item["type"], default=item["default"], nargs=item["cont"])
 for item in flag:
   pars.add_argument("--"+item['name'], help=item['desc'], action="store_true")
-if not os.isatty(sys.stdin.fileno()): 
+parg = [] 
+if not os.isatty(sys.stdin.fileno()):
   for line in list(sys.stdin):
-    args = pars.parse_args((sys.argv[:1] + line.rstrip().split() + sys.argv[1:])[1:])
-    print('%.16G' %(math.sqrt(getattr(args,"dx")[0]**2+getattr(args,"dy")[0]**2+getattr(args,"dz")[0]**2)))
-  exit(0)
-args = pars.parse_args(sys.argv[1:])
+    parg.append(line.rstrip().split() + list(filter(lambda item: '-' in item or '--' in item, sys.argv[1:])))
+    pars.parse_args(parg[-1])
+else:
+  parg.append(sys.argv[1:])
+  pars.parse_args(parg[-1])
 
 # implementation
-print('%.16G' %(math.sqrt(getattr(args,"dx")[0]**2+getattr(args,"dy")[0]**2+getattr(args,"dz")[0]**2)))
+out0 = []
+inp0 = []
+for item in parg:
+  args = pars.parse_args(item)
+  out0.append([math.sqrt(getattr(args,"dx")[0]**2+getattr(args,"dy")[0]**2+getattr(args,"dz")[0]**2)])
+
+# output
+tabl = prettytable.PrettyTable()
+tabl.set_style(prettytable.MARKDOWN)
+tabl.field_names = ["dx", "dy", "dz", "MRE"]
+if "--human" in sys.argv:
+  for i in range(len(out0)):
+    for j in range(len(out0[i])):
+      tabl.add_row(["%s"%parg[i][0],"%s"%parg[i][1],"%s"%parg[i][2],"%.3f"%out0[i][j]])
+  print("\n%s"%tabl)
+else:
+  numpy.savetxt(sys.stdout,out0,fmt='%.6G', delimiter="\n")
