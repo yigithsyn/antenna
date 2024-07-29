497f4b641fcf03067f6a13131ae6d5ca3e824951
Hüseyin YİĞİT <yigit.hsyn@gmail.com>
2024-07-29 14:09:34 +0300

    function add

--- scripts/propagation/pathLoss.py
+++ scripts/propagation/pathLoss.py
@@ -34,27 +34,18 @@ exec(tmpl)
 inps = []
 for item in parg:
   args = pars.parse_args(item)
-  # print(getattr(args,"R"))
-  # print(getattr(args,"G1"))
   inps = numpy.append(inps,[getattr(args,"frequency"),getattr(args,"R"),[getattr(args,"G1")],[getattr(args,"G2")]])
-  # inps.append(numpy.asarray(getattr(args,"frequency")),getattr(args,"R"),getattr(args,"G1"),getattr(args,"G2"))
 inps = numpy.reshape(inps, (-1,4))
 outs = 32.45 + 20*numpy.log10(inps[:,0]*inps[:,1]) - inps[:,2] - inps[:,3]
-print(outs)
-# out0 = []
-# inp0 = []
-# for item in parg:
-#   args = pars.parse_args(item)
-#   out0.append([math.sqrt(getattr(args,"dx")[0]**2+getattr(args,"dy")[0]**2+getattr(args,"dz")[0]**2)])
 
-# # output
-# tabl = prettytable.PrettyTable()
-# tabl.set_style(prettytable.MARKDOWN)
-# tabl.field_names = ["dx", "dy", "dz", "MRE"]
-# if "--raw" not in sys.argv:
-#   for i in range(len(out0)):
-#     for j in range(len(out0[i])):
-#       tabl.add_row(["%s"%parg[i][0],"%s"%parg[i][1],"%s"%parg[i][2],"%.3f"%out0[i][j]])
-#   print("\n%s"%tabl)
-# else:
-#   numpy.savetxt(sys.stdout,out0,fmt='%.6G', delimiter="\n")
+# output
+tabl = prettytable.PrettyTable()
+tabl.set_style(prettytable.MARKDOWN)
+tabl.field_names = ["frequency [MHz]", "ditance [km]", "transmitter antenna gain [dB]", "receiver antenna gain [dB]", "path loss [dB]"]
+if "--raw" not in sys.argv:
+  for i, item in enumerate(outs):
+    tabl.add_row(["%.3f"%inps[i][0],"%.3f"%inps[i][1],"%.1f"%inps[i][2],"%.1f"%inps[i][3],"%.2f"%item])
+  print("\n%s"%tabl)
+else:
+  numpy.savetxt(sys.stdout,outs,fmt='%.6G', delimiter="\n")
+
